c3d886e7f9542ac2d6af80ab92fad7bcd994734f
Dawid Pawulski <dpawulski@gmail.com>
2021-06-20 21:21:00 +0200

    Create CreateCategoryCommandValidatorTests.cs

---
+++ NotebookBackend/NotebookAPITests/ValidationTests/Categories/CreateCategoryCommandValidatorTests.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using NotebookAPI.Commands.Categories;
+using NotebookAPI.Validation.Categories;
+using NUnit.Framework;
+
+namespace NotebookAPITests.ValidationTests.Categories
+{
+    [TestFixture]
+    public class CreateCategoryCommandValidatorTests
+    {
+        [Test]
+        public async Task CreateCategoryCommandValidator_CreateCategoryWithName_ShouldBeValidatedToTrue()
+        {
+            var validator = new CreateCategoryCommandValidator();
+            var createCategoryCommand = new CreateCategoryCommand
+            {
+                Name = "category name"
+            };
+
+            var validationResult = await validator.ValidateAsync(createCategoryCommand);
+
+            Assert.True(validationResult.IsValid);
+        }
+        
+        [Test]
+        public async Task CreateCategoryCommandValidator_CreateCategoryToEmptyName_ShouldBeValidatedToFalse()
+        {
+            var validator = new CreateCategoryCommandValidator();
+            var createCategoryCommand = new CreateCategoryCommand
+            {
+                Name = ""
+            };
+
+            var validationResult = await validator.ValidateAsync(createCategoryCommand);
+
+            Assert.False(validationResult.IsValid);
+        }
+        
+        [Test]
+        public async Task CreateCategoryCommandValidator_CreateCategoryNameWithForbiddenCharacters_ShouldBeValidatedToFalse()
+        {
+            var validator = new CreateCategoryCommandValidator();
+            var createCategoryCommand = new CreateCategoryCommand
+            {
+                Name = "Home!"
+            };
+
+            var validationResult = await validator.ValidateAsync(createCategoryCommand);
+
+            Assert.False(validationResult.IsValid);
+        }
+    }
+}
