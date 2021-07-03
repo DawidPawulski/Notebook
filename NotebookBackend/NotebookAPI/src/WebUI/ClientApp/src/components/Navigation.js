f37481a231c0b63115385419a055d4f573d4a614
Dawid Pawulski <dpawulski@gmail.com>
2021-07-03 17:47:34 +0200

    Add ClientApp

---
+++ NotebookBackend/NotebookAPI/src/WebUI/ClientApp/src/components/Navigation.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+import {NavLink} from 'react-router-dom';
+import {Navbar,Nav} from 'react-bootstrap';
+
+export class Navigation extends Component{
+
+    render(){
+        return(
+            <Navbar bg="dark" expand="lg">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-nabar-nav">
+                    <Nav>
+                        <NavLink className="d-inline p-2 bg-dark text-white" to="/">
+                            Note list
+                        </NavLink>
+                        <NavLink className="d-inline p-2 bg-dark text-white" to="/category-list">
+                            Category list
+                        </NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
